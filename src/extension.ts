b3b090abc8aca03f4554771ebd532cc48d42b18f
caoayu <1401262639@qq.com>
2023-06-11 00:54:32 +0800

    👌 IMPROVE:

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,49 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.toggleCase', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const selection = editor.selection;
-            const text = editor.document.getText(selection);
-            const toggledText = toggleCase(text);
-            editor.edit(editBuilder => {
-                editBuilder.replace(selection, toggledText);
-            });
-        }
-    });
+  let disposable = vscode.commands.registerCommand(
+    "extension.toggleCase",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
+        let range: vscode.Range | undefined;
+        let text: string = "";
 
-    context.subscriptions.push(disposable);
+        if (!selection.isEmpty) {
+          // 如果有选中文本，则切换选中文本的大小写
+          range = selection;
+          text = document.getText(range);
+        } else {
+          // 如果没有选中文本，则切换光标所在位置的单词的大小写
+          const position = selection.active;
+          const wordRange = document.getWordRangeAtPosition(position);
+          if (wordRange) {
+            range = wordRange;
+            text = document.getText(range);
+          }
+        }
+
+        if (range && text) {
+          const toggledText = toggleCase(text);
+          editor.edit((editBuilder) => {
+            editBuilder.replace(range!, toggledText);
+          });
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 function toggleCase(text: string): string {
-    console.log(text);
-	if (text === text.toUpperCase()) {
-		return text.toLowerCase();
-	} else if (text === text.toLowerCase()) {
-		return text.toUpperCase();
-	} else {
-		return text.toLowerCase();
-	}
+  if (text === text.toUpperCase()) {
+    return text.toLowerCase();
+  } else if (text === text.toLowerCase()) {
+    return text.toUpperCase();
+  } else {
+    return text.toLowerCase();
+  }
 }
